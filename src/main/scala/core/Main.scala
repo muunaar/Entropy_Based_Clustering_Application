bcd2e53dea0640d2d22016724c59afad6f5eb321
muunaar <amroumouna@gmail.com>
2020-09-17 15:22:33 +0100

    feat: edit Main

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -8,16 +8,28 @@ import io.chrisdavenport.log4cats.Logger
 
 object Main extends IOApp {
 
+  import util.FileUtilities._
+  import util.HelperForMergingAllCalculations._
+  import util.Exceptions._
+
   def subCall(logger: Logger[IO]): IO[Unit] = {
     for {
       _ <- logger.info("Starting Entropy clustering Application")
       _ <- logger.info("User is asked to provide clustering threshold value") *>
         logger.info("It should be < 0.9 and > 0.5")
 
-      _ <- logger.trace(s"Receiving the clustering threshold Beta")
+      beta <- readBeta
+      _ <- logger.trace(s"Receiving the clustering threshold Beta = $beta")
+      _ <- if (compareBetaToRange(beta)) {
+        logger.info("Similarity threshold received successfully")
+      } else {
+        IO.raiseError(ThresholdNotInRange)
+      }
 
       _ <- logger.info("Reading the file path")
-      _ <- logger.trace(s"Receiving file path ")
+      path <- getPath
+      _ <- logger.trace(s"Receiving file path = $path")
+      _ <- fileToClusters(path, beta)(logger)
       _ <- logger.info("Finished")
     } yield ()
   }
@@ -27,6 +39,13 @@ object Main extends IOApp {
     for {
       logger <- Slf4jLogger.create[IO]
       x <- subCall(logger)
+        .handleErrorWith {
+          case ThresholdNotInRange =>
+            logger.error(ThresholdNotInRange.toString)
+          case FileNotCorrectlyFormated =>
+            logger.error(FileNotCorrectlyFormated.toString)
+          case _ => logger.error("Unexpected Error has occured")
+        }
         .as(ExitCode.Success)
         .guaranteeCase {
           case Canceled =>
