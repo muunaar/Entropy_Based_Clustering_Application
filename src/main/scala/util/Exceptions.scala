a308a47f6858a039b58edcaeb650e45cdd23ca51
muunaar <amroumouna@gmail.com>
2020-09-17 14:56:59 +0100

    feat: define Exceptions

---
+++ src/main/scala/util/Exceptions.scala
@@ -0,0 +1,24 @@
+package util
+
+object Exceptions {
+
+  /**
+    * Exception for handling similarity threshold!
+    * Thrown when the value inserted does not fit in the range.
+    */
+  case object ThresholdNotInRange extends Exception {
+    override def toString: String =
+      "Similarity threshold Not in range : It should be < 0.9 and > 0.5"
+  }
+
+  /**
+    * Exception for handling data file formatting
+    * Thrown when the File is not correctly formatted according to (R  A,A,A)
+    * Where R is the resource performing the task and A the related tasks
+    */
+  case object FileNotCorrectlyFormated extends Exception {
+    override def toString: String =
+      "Error Parsing : File formatting is not respected "
+  }
+
+}
